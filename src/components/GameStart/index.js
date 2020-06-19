702c8773c5973f874e19cadea1d1b953b5352fb6
corbineau <lochinvar@gmail.com>
2020-06-19 00:31:19 -0400

    react game engine stuff. Materialize sucks tho

--- src/components/GameStart/index.js
+++ src/components/GameStart/index.js
@@ -4,7 +4,7 @@ class GameStart extends Component {
     render() {
       return (
         <button className="startButton" onClick={() => 
-        
+        console.log()
         }>
           {this.props.value}
         </button>
---
+++ src/components/renderers.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+
+class Domino extends PureComponent {
+  render() {
+    const len = 100;
+    const wid = 50;
+    const x = this.props.x - size / 2;
+    const y = this.props.y - size / 2;
+    return (
+      <div style={{ position: "absolute", width: wid, height: len, backgroundColor: "white", left: x, top: y }} />
+    );
+  }
+}
+
+export { Domino };
