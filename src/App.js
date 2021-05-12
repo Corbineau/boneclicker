43621e2415de40bccc6120b165e511544fdbd596
corbineau <lochinvar@gmail.com>
2021-05-12 01:31:33 -0400

    fixed a couple of build bugs, and now things build! Huzzah :q!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SimpleGame from './components/game';
+import SimpleGame from './components/Game';
 
 function App() {
   return (
--- src/components/renderers.js
+++ src/components/renderers.js
@@ -4,8 +4,8 @@ class Domino extends PureComponent {
   render() {
     const len = 100;
     const wid = 50;
-    const x = this.props.x - size / 2;
-    const y = this.props.y - size / 2;
+    const x = this.props.x - 2;
+    const y = this.props.y - 2;
     return (
       <div style={{ position: "absolute", width: wid, height: len, backgroundColor: "white", left: x, top: y }} />
     );
